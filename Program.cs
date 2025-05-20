b20d2cb1f94d04b0ef281830ed47a52de85fc916
hershy rozenfeld <Y6764057@gmail.com>
2025-05-20 15:43:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Battle_Arena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
